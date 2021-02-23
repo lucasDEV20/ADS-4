9e4c0bc87050da77b15cbcc5b52d19c78c2c758f
lucasDEV20 <lucasgoias11@gmail.com>
2021-02-22 23:44:52 -0300

    aprimorando exercicio Pessoa

--- ADS-4/DW/backend/src/main/java/com/lucas/backend/entidade/Curso.java
+++ ADS-4/DW/backend/src/main/java/com/lucas/backend/entidade/Curso.java
@@ -1,5 +1,39 @@
 package com.lucas.backend.entidade;
 
-public class Curso {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+public class Curso {
+	
+	@Entity
+	public class Cliente {
+		
+		@Id
+		@GeneratedValue(strategy = GenerationType.IDENTITY)
+		private Long id;
+		
+		@Column(nullable = false)
+		private String nome;
+
+		public Long getId() {
+			return id;
+		}
+
+		public void setId(Long id) {
+			this.id = id;
+		}
+
+		public String getNome() {
+			return nome;
+		}
+
+		public void setNome(String nome) {
+			this.nome = nome;
+		}
+
+
+}
 }
